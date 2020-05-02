07d8f5eeeba3ae4a99acbe9de7cf7c85e2c61369
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-02 10:00:24 -0500

    set up local storage for stored li cities in ul

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-$(document).ready(function(){
-  // This function handles events where one button is clicked
-      $("#searchButton").on("click", function(event) {
-       if ($("#cityStateInput").val() !== "" ) {
-            event.preventDefault();
-            var li = $("<li>");
-            var newCity = $("#cityStateInput").val()
-            li.addClass("list-group-item");
-            li.attr("data-name", newCity);
-            li.text(newCity);
-            $("#cityStateList").append(li);
-            
-       }
-        
-      });
-
-      
-
-})
+// variable to hold the list of cities in search
+var savedCitiesList;
+if (localStorage.savedCitiesList === undefined) {
+    savedCitiesList = [];
+} else {
+    savedCitiesList = JSON.parse(window.localStorage.savedCitiesList);
+}
 
 
+// This function handles the event of adding a city to the savedCitiesList array
+function searchAndSave(event) {
+    if ($("#cityStateInput").val() !== "") {
+        event.preventDefault();
+        var newCity = $("#cityStateInput").val();
+        savedCitiesList.push(newCity);
+        localStorage.setItem("savedCitiesList", JSON.stringify(savedCitiesList));
+        createCitiesStateList();
+        weatherForcast(newCity);
+    }
+};
+// this function handles looking at local storage and prepending new items to the search list.
+function createCitiesStateList() {
+    $("#cityStateList").empty();
+    for (let i = 0; i < savedCitiesList.length; i++) {
+        var li = $("<li>");
+        li.addClass("list-group-item");
+        li.attr("data-name", savedCitiesList[i]);
+        li.text(savedCitiesList[i]);
+        $("#cityStateList").prepend(li);
+    }
+}
+// call function to create list of sities if they exist in local storage
+createCitiesStateList()
+
+// this function allows user to clear the list of cities from local storage.
+function clearCityStateList(event) {
+    event.preventDefault();
+    var youSure = confirm("Are you sure you want to clear search history")
+    if (youSure === true) {
+        localStorage.clear();
+        location.reload();
+    } else {
+        return
+    }
+}
+
+function weatherForcast(newCity) {
+    console.log(newCity);
+}
+
+
+
+// event listeners 
+$("#searchButton").on("click", searchAndSave)
+$("#clearSearch").on("click", clearCityStateList)
 
